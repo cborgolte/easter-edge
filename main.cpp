ac1cdfa1c266f5c5e49f6c30ef2db62a4b2d7948
Christoph Borgolte <christoph.borgolte@bayard-consulting.com>
2017-04-15 22:55:44 +0200

    Implemented getOrInsertNode

--- main.cpp
+++ main.cpp
@@ -24,70 +24,64 @@ std::string join(const T& v, const std::string& delim) {
 }
 
 
+std::pair<bool, size_t> getOrInsertNode(const std::string& nodeName, std::vector<std::string>& nodes) {
+    auto index = std::find(nodes.begin(), nodes.end(), nodeName);
+    if (index == nodes.end()) {
+        nodes.push_back(nodeName);
+        // inserted
+        return std::make_pair(true, nodes.size());
+    }
+    // no new node
+    return std::make_pair(false, index - nodes.begin());
+}
+
+
 int main()
 {
     crow::SimpleApp app;
 
     Graph graph;
     std::vector<std::string> nodes;
-    std::vector<std::pair<std::string, std::string>> edges;
 
-    CROW_ROUTE(app, "/nodes")
-        ([&nodes](){
-         //std::ostringstream os;
-         //os << join(nodes, ", ");
-         //return crow::response{os.str()};
-
+    CROW_ROUTE(app, "/nodes")([&nodes](){
          crow::json::wvalue resp;
          resp["nodes"] = nodes;
          return resp;
+     });
 
-         });
-
-    CROW_ROUTE(app, "/edges")
-        .methods("POST"_method)
-        ([&nodes, &edges, &graph](const crow::request& req){
+    CROW_ROUTE(app, "/edges").methods("POST"_method)
+        ([&nodes, &graph](const crow::request& req){
          auto doc = crow::json::load(req.body);
 
          crow::json::wvalue resp;
 
          if (!doc) {
-         resp["status"] = 400;
-         return resp;
+             resp["status"] = 400;
+             return resp;
          }
+
          std::vector<std::string> nodesCreated;
+
          std::string in = doc["in"].s();
          std::string out = doc["out"].s();
-         size_t inPos, outPos;
-         auto index = std::find(nodes.begin(), nodes.end(), in);
-         if (index == nodes.end()) {
-             nodes.push_back(in);
-             nodesCreated.push_back(in);
-             inPos = nodes.size();
+
+         auto inNode = getOrInsertNode(in, nodes);
+         auto outNode = getOrInsertNode(out, nodes);
+
+         if (inNode.first) {
+            nodesCreated.push_back(in);
          }
-         else {
-             inPos = index - nodes.begin();
+
+         if (outNode.first) {
+            nodesCreated.push_back(out);
          }
-         index = std::find(nodes.begin(), nodes.end(), out);
-         if (index == nodes.end()) {
-             nodes.push_back(out);
-             nodesCreated.push_back(out);
-             outPos = nodes.size();
-         }
-         else {
-             outPos = index - nodes.begin();
-         }
-         add_edge(inPos, outPos, graph);
-         auto edge = std::make_pair(in, out);
-         if (std::find(edges.begin(), edges.end(), edge) == edges.end()) {
-             edges.push_back(edge);
-         }
+
+         auto res = add_edge(inNode.second, outNode.second, graph);
 
          resp["new_nodes"] = nodesCreated;
-         resp["edge"] = new std::vector<std::string>({edge.first, edge.second});
+         //resp["edge"] = res;
          return resp;
-
-        });
+    });
 
     app.port(18080).multithreaded().run();
 }

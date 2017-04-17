21f58a383a7c99dc2303fc969221f2df964b39aa
Christoph Borgolte <christoph.borgolte@bayard-consulting.com>
2017-04-17 13:06:54 +0200

    Check results of load test.

---
+++ wrk/check.py
@@ -0,0 +1,29 @@
+import requests
+
+"""
+Run this script after running wrk.sh to validate that all nodes were
+generated.
+"""
+
+nodes = requests.get('http://localhost:18080/nodes').json()
+
+arr = nodes['nodes']
+
+all_in1 = [n for n in arr if n.startswith('in_1')]
+all_in1.sort()
+
+v1, *vals = all_in1
+
+deltas = []
+missing = []
+for v2 in vals:
+    delta = int(v2[3:]) - int(v1[3:])
+    if (delta != 1):
+        missing.append((v1, v2))
+    deltas.append(delta)
+    v1 = v2
+
+print(deltas)
+print(missing)
+print(sum(deltas))
+print(len(arr))
